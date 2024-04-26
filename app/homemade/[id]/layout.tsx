b98a0f91bf15727541aa5ce56214e16aa3790a08
syab-syab <syabsyabsyabsyab@gmail.com>
2024-04-26 18:23:42 +0900

    generateMetadata設定完了

--- app/homemade/[id]/layout.tsx
+++ app/homemade/[id]/layout.tsx
@@ -1,3 +1,15 @@
+import data from "../../data/app-data.json"
+import { appType } from "@/app/types//All.types"
+
+
+export function generateMetadata({ params }: { params: { id: number } }) {
+  const appData: appType | any = data["app"].find(d => d.id == params.id)
+
+  return {
+    title: `${appData.name} | スイスアーミーアプリ`,
+  }
+}
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
